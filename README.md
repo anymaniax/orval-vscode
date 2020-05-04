cebeba4999d374010e056c50ad2a3916400fdcde
Victor Bury <bury.victor@gmail.com>
2020-05-04 13:06:17 +0200

    feat(commands): add generate and generate all

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <img src="/logo/orval-logo-horizontal.png?raw=true" width="500" height="160" alt="Orval - Restfull Client Generator" />
+</p>
+
+### Code Generation
+
+`orval` is able to generate axios client with appropriate type-signatures (TypeScript) from any valid OpenAPI v3 or Swagger v2 specification, either in `yaml` or `json` formats.
+
+## Installation
+
+Install through VS Code extensions. Search for `orval`
+
+[Visual Studio Code Market Place: orval](https://marketplace.visualstudio.com/items?itemName=orval)
+
+Can also be installed in VS Code: Launch VS Code Quick Open (Ctrl+P), paste the following command, and press enter.
+
+```
+ext install orval
+```
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+import orval from 'orval';
+import { dirname, join, relative } from 'path';
+import * as vscode from 'vscode';
+import { window } from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  const generate = vscode.commands.registerCommand(
+    'extension.generate',
+    async () => {
+      const files = await vscode.workspace.findFiles('**/{orval.config.js}');
+      const paths = files.map(({ path }) =>
+        dirname(relative(vscode.workspace.rootPath || '', path)),
+      );
+
+      if (paths.length === 1) {
+        orval(
+          join(vscode.workspace.rootPath || '', paths[0], './orval.config.js'),
+        );
+        return;
+      }
+
+      window.showQuickPick(paths, {
+        placeHolder: 'select a project',
+        onDidSelectItem: (item: string) =>
+          orval(
+            join(vscode.workspace.rootPath || '', item, './orval.config.js'),
+          ),
+      });
+    },
+  );
+
+  const generateAll = vscode.commands.registerCommand(
+    'extension.generateAll',
+    () => {
+      const files = vscode.workspace.findFiles('**/{orval.config.js}');
+
+      files.then((uris) => {
+        uris.forEach((uri) => {
+          orval(uri.path);
+        });
+      });
+    },
+  );
+
+  context.subscriptions.push(generateAll);
+}
+
+export function deactivate() {}
