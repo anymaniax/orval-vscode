2cb52b8987bc552fd847534a0e0930ca95e17495
anymaniax <bury.victor@gmail.com>
2021-08-31 12:05:22 +0200

    fix(package): build and new design

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,30 @@
 import orval from 'orval';
 import { dirname, join, relative } from 'path';
 import * as vscode from 'vscode';
-import { window } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   const generate = vscode.commands.registerCommand(
     'extension.generate',
     async () => {
-      const files = await vscode.workspace.findFiles('**/{orval.config.js}');
-      const paths = files.map(({ path }) =>
-        dirname(relative(vscode.workspace.rootPath || '', path)),
-      );
-
-      if (paths.length === 1) {
-        orval(
-          join(vscode.workspace.rootPath || '', paths[0], './orval.config.js'),
+      vscode.workspace.workspaceFolders?.forEach(async (folder) => {
+        const files = await vscode.workspace.findFiles('**/{orval.config.js}');
+        const paths = files.map(({ path }) =>
+          dirname(relative(folder.uri.path || '', path)),
         );
-        return;
-      }
 
-      const item = await window.showQuickPick(paths, {
-        placeHolder: 'select a orval config',
+        if (paths.length === 1) {
+          orval(join(folder.uri.path || '', paths[0], './orval.config.js'));
+          return;
+        }
+
+        const item = await vscode.window.showQuickPick(paths, {
+          placeHolder: 'select a orval config',
+        });
+
+        if (item) {
+          orval(join(folder.uri.path || '', item, './orval.config.js'));
+        }
       });
-
-      if (item) {
-        orval(join(vscode.workspace.rootPath || '', item, './orval.config.js'));
-      }
     },
   );
 
@@ -45,5 +44,3 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(generate);
   context.subscriptions.push(generateAll);
 }
-
-export function deactivate() {}
